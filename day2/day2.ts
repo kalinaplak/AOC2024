30701d2aa7725b055ff3dddcdf62b30ac44c1b8c
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2024-12-09 07:41:11 +0100

    day2

---
+++ day2/day2.ts
@@ -0,0 +1,63 @@
+const fs = require('fs');
+import { count, map, pairwise, pipe, toarray } from 'powerseq';
+
+function parseInput(input: string) {
+  return pipe(
+    input.split(/\n/),
+    map(l => l.split(' ').map(Number)),
+    toarray()
+  );
+}
+
+function mapToSafe(input: number[][]) {
+  return pipe(
+    input,
+    map(line =>
+      pipe(
+        pairwise(line),
+        map(([n1, n2]) => n1 - n2),
+        toarray()
+      )
+    ),
+    map(line => (line.every(n => n > 0) || line.every(n => n < 0)) && line.every(n => {
+      const abs = Math.abs(n);
+      return abs > 0 && abs < 4;
+    })),
+    toarray()
+  )
+}
+
+//1.
+const input = parseInput(fs.readFileSync('./day2/input.txt', 'utf-8'));
+
+console.log(input);
+
+function part1(input: number[][]) {
+  return count(mapToSafe(input), l => !!l);
+}
+
+//2.
+function removeElementAtIndex(arr: number[], index: number): number[] {
+  return [...arr.slice(0, index), ...arr.slice(index + 1)];
+}
+
+function part2(input: number[][]) {
+  const mappedInput = mapToSafe(input);
+  return pipe(
+    input,
+    count((r, i) => mappedInput[i] ? true : r.some((_, j) => {
+      const withoutCol = removeElementAtIndex(r, j);
+      return mapToSafe([withoutCol])[0];
+    })),
+  )
+}
+
+console.log(part1(input));
+console.log(part2(input));
+
+
+
+
+
+
+
